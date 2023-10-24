c51588476599738ae7e2d264c4e391d76f8e85fd
Jeff Baumgardt <medros1455@gmail.com>
2023-10-24 12:38:37 -0600

    Add options as second argument

--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -1,12 +1,16 @@
-const selectorList = ["p"];
-const prefixLength = 3;
-
 /**
  * Implement your converter function here.
  */
-const diyOnicConverter = (textContentContainerSelector) => {
+const diyOnicConverter = (textContentContainerSelector, options = {}) => {
+  const {
+    selectorList = ["p"],
+    prefixLength = 3,
+    formatType = "bold",
+  } = options;
   const container = document.querySelector(textContentContainerSelector);
   console.log("Performing bionic reading conversion on:", container);
+
+  console.log(formatType);
 
   const selectedElements = selectorList
     .map((selector) => Array.from(container.querySelectorAll(selector)))
@@ -20,11 +24,20 @@ const diyOnicConverter = (textContentContainerSelector) => {
 
     elementText.split(" ").forEach((text) => {
       // Create two nodes, the outer for containing the entire text and the inner for bolding. HTML 5 lets us
-      // us create any element we want so `text` will work just fine and the `b` will be bold per html spec.
-      // But for grins we'll add some css selectors and style it as well.
+      // us create any element we want so `text` will work just fine and the `innerText` will be bold per formayType style.
       const textNode = document.createElement("text");
-      const newText = document.createElement("b");
-      newText.style = "font-weight: bold;";
+      const newText = document.createElement("innerText");
+      switch (formatType) {
+        case "bold":
+          newText.style = "font-weight: bold;";
+          break;
+        case "italic":
+          newText.style = "font-style: italic;";
+          break;
+        case "underline":
+          newText.style = "text-decoration: underline;";
+          break;
+      }
 
       // Grab the prefix length of the text to create the new bold node
       newText.textContent = text.slice(0, prefixLength);
@@ -40,3 +53,5 @@ const diyOnicConverter = (textContentContainerSelector) => {
 
 // Allow global access so that this can be executed from the console.
 window.diyOnicConverter = diyOnicConverter;
+
+function addControlsOverlay() {}
