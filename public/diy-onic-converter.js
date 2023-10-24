44cbb5e76a8677d4b599beb7ecb61ea58b021c31
Jeff Baumgardt <medros1455@gmail.com>
2023-10-24 11:39:55 -0600

    Initial pass. This will create new text nodes with bold nodes inside and append the rest of the text. This will rebuild the entire selectorList.

--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -1,3 +1,6 @@
+const selectorList = ["p"];
+const prefixLength = 3;
+
 /**
  * Implement your converter function here.
  */
@@ -5,7 +8,29 @@ const diyOnicConverter = (textContentContainerSelector) => {
   const container = document.querySelector(textContentContainerSelector);
   console.log("Performing bionic reading conversion on:", container);
 
-  /* Etc. etc. etc. your code etc. */
+  const selectedElements = Array.from(
+    selectorList.map((selector) =>
+      Array.from(container.querySelectorAll(selector))
+    )
+  ).flat();
+  // console.log(selectedElements);
+
+  selectedElements.forEach((element) => {
+    let returnContent = document.createElement(element.tagName);
+    const elementText = element.textContent;
+
+    elementText.split(" ").forEach((text) => {
+      const textNode = document.createElement("text");
+      const newText = document.createElement("b");
+
+      newText.textContent = text.slice(0, prefixLength);
+      textNode.appendChild(newText);
+      textNode.append(text.slice(prefixLength, text.length) + " ");
+
+      returnContent.appendChild(textNode);
+      container.appendChild(returnContent);
+    });
+  });
 };
 
 // Allow global access so that this can be executed from the console.
